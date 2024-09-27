13e3b05e4e240e87d42d8a552b69fe4fceb74bc6
Ganesh gowda <gowdaganesh0511@gmail.com>
2024-09-28 00:16:42 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const ButtonClick = () => {
+  console.log("Button Clicked");
+};
+
+const Home = () => {
+  return (
+    <div className="relative items-center w-screen h-screen bg-black">
+      {/* Spline iframe */}
+      <iframe
+        className="absolute w-screen h-screen top-1 left-0"
+        src="https://my.spline.design/robotfollowcursorforlandingpage-368e9926666555d919c7a83361a7a50c/"
+        
+        allowFullScreen
+        style={{ border: "none" }}
+      />
+
+      {/* Top Text */}
+      <div className="absolute font-oswald top-5 w-full text-center text-7xl md:text-5xl bg-gradient-to-r from-purple-900 to-blue-200 bg-clip-text text-transparent">
+        Experience The Power
+      </div>
+
+      <div className="absolute font-oswald left-20  top-10 w-20 text-left text-5xl md:text-3xl text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, atque?
+      </div>
+
+      {/* Register Button at the bottom */}
+      <div className="absolute bottom-10 w-full flex justify-center">
+        <button
+          onClick={ButtonClick}
+          className="bg-gradient-to-r
+          from-purple-600 to-blue-400  text-black text-2xl font-oswald rounded-3xl px-8 py-3 hover:bg-gray-300"
+          
+        >
+          Register Today
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      textFillColor: theme => theme('colors'),
+      fontFamily: {
+        oswald: ['Oswald','sans-serif'],
+        shad: ["Shadows Into Light", "cursive"],
+      }
+    },
+  },
+  plugins: [
+   
+  ],
+}
+
